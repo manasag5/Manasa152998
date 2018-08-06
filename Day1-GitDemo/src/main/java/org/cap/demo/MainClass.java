6491ed7a65dfb5d68c94c9d2d0140492c534062d
mgundaji <mgundaji@DIN73002988.corp.capgemini.com>
2018-08-06 10:41:02 +0530

    my first commit

---
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,13 @@
+package org.cap.demo;
+
+public class MainClass {
+public static void main(String args[]) {
+	System.out.println("HelloWorld");
+	greet();
+}
+
+public static void greet() {
+	// TODO Auto-generated method stub
+	System.out.println("hello");
+}
+}

e3e7edbf6770c980eae047b5f17b8c36a95fd362
mgundaji <mgundaji@DIN73002988.corp.capgemini.com>
2018-08-06 10:52:55 +0530

    second committ

--- Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -9,5 +9,7 @@ public static void main(String args[]) {
 public static void greet() {
 	// TODO Auto-generated method stub
 	System.out.println("hello");
+	System.out.println("capgemini");
+	System.out.println("first git demo");
 }
 }
